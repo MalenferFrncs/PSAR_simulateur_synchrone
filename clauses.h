6a877901a8860b86713ead7a1ee7a0019949b181
Malenferfrncs <francois.malenfer@gmail.com>
2024-02-16 12:37:05 +0100

    ajout des premiers fichier .h dans lesqules on decrits les topo e(test)

---
+++ clauses.h
@@ -0,0 +1,11 @@
+#ifndef CLAUSES_H
+#define CLAUSES_H
+
+#define NBCLAUSE 4
+
+
+
+
+
+
+#endif
---
+++ topologie.h
@@ -0,0 +1,12 @@
+#ifndef TOPOLOGIE_H
+#define TOPOLOGIE_H
+
+#define NBSOMMET 4
+int topologie[NBSOMMET][NBSOMMET]= {
+    {1,0,1,0},
+    {0,1,0,0},
+    {1,0,1,0},
+    {0,0,0,1}
+};
+
+#endif
