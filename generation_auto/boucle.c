9d20c59764069ac4d84a1e3ae5a697317703b74f
Malenferfrncs <francois.malenfer@gmail.com>
2024-02-28 21:33:23 +0100

    ajout de la generation auto de la topologie (actuelement juste dans le fichier generation auto qui implemente le meme exemple que le 3) TODO faire une sauvegarde des topo passé et arreter la boucle quand on revoit une configuration

---
+++ generation_auto/boucle.c
@@ -0,0 +1,88 @@
+#include "make_topo.h"
+#include "clauses.h"
+#include "macro.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void eval_macro(FILE* sortie,int **topologie, int **var,int **macro_mem, int nbNode, int nbVar){
+    for(int i = 0 ; i<nbNode; i++){
+        fprintf(sortie,"\t\tsommet %d",i);
+        
+        for(int j = 0 ; j< NBMACRO; j++){
+            int res = macro[j](i,topologie,var,macro_mem,nbNode,nbVar);
+            
+            macro_mem[i][j] = res;
+            
+            fprintf(sortie," Token : %d,  v : %d \n",macro_mem[i][j],var[i][0]);
+        }
+    }
+}
+
+void eval_gardes(FILE* sortie,int **topologie, int **var, int nbNode, int nbVar,int **macro_mem,int * action_id){
+    for(int i=0;i<nbNode;i++){
+        for(int j=0; j<NBCLAUSE; j++){
+            int res = (gardes[j])(i,topologie,var,nbNode,nbVar,macro_mem,action_id);
+            if( res ){
+                action_id[i] = j;
+                /* fprintf(sortie,"\t\tle sommet : %d fera l'action : %s \n",i,nom_des_actions[j]); */
+            }
+        }
+    }
+}
+
+void eval_action(FILE* sortie,int **topologie, int **var, int nbNode, int nbVar,int **macro_mem,int *action_id){
+    for(int i=0;i<nbNode;i++){
+        action[action_id[i]](i,topologie,var,nbNode,nbVar,macro_mem,action_id);
+        fprintf(sortie,"\t\tle sommet %d a fait l'action %s \n",i,nom_des_actions[action_id[i]]);
+    }
+}
+
+
+void eval(FILE* sortie,int **topologie, int **var, int nbNode, int nbVar){ /* si on veut tourner a l'infini nb_cycles = -1*/
+    int i = 1;
+    int **macro_mem = (int**)malloc(sizeof(int*)*nbNode);
+    
+    for(int i=0; i<nbNode; i++){
+        
+        *(macro_mem+i) = (int*)malloc(sizeof(int)*NBMACRO);
+                
+    }
+    int action_id[nbNode] ;
+    while(i <= 100){
+        fprintf(sortie,"\n\ndebut de l'évaluation du cycles %d \n \n",i);
+        fprintf(sortie,"\n\tvaleurs des macro et des memoires  \n \n");
+        eval_macro(sortie,topologie,var,macro_mem,nbNode,nbVar);
+        
+        /* fprintf(sortie,"\n\tdebut de l'évaluation des gardes du cycles %d \n \n",i); */
+        eval_gardes(sortie,topologie,var,nbNode,nbVar,macro_mem,action_id);
+        fprintf(sortie,"\n\tdebut des actions du cycles %d \n \n",i);
+        eval_action(sortie,topologie,var,nbNode,nbVar,macro_mem,action_id);
+        i++;
+    }
+}
+
+int main(){
+
+    int **topo;
+    int **var;
+    FILE * topo_file = fopen("exemple_topo.txt","r");
+    FILE * var_file = fopen("exemple_var.txt","r");
+    int taille;
+    int nbvar;
+
+    taille = make_topo(&topo,topo_file);
+    nbvar = make_var(&var,var_file);
+
+    fclose(topo_file);
+    fclose(var_file);
+
+    
+
+    FILE *sortie = fopen("example_3_Token_ring.txt","w");
+    if(sortie==NULL){
+        printf("problème pendant l'ouverture du fichier de trace");
+    }
+    
+    eval(sortie,topo,var,taille,nbvar);
+    return 0;
+}
